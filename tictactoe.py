b2c1fa2dc7504d6cef85a153ed64e30394d412f5
Kevinoshea <kevinbarretoshea@gmail.com>
2020-04-30 01:28:35 -0400

    Added logic

--- tictactoe.py
+++ tictactoe.py
@@ -3,16 +3,10 @@ import random
 import subprocess
 
 
-board = ['A','A','A','A','A','A','A','A','A']
+# board = ['A','A','A','A','A','A','A','A','A']
+board = ['X','X','X','A','A','A','A','A','A']
 
 class play:
-
-    def place_piece(piece, place):
-        board[place] = piece
-        if(board[place] == piece):
-            return true
-        return false
-
 
     def print_board_data():
         for i in range(len(board)):
@@ -20,7 +14,8 @@ class play:
             if(i != 0 and (i == 2 or i == 5 or i == 8)):
                 print('')
 
-    def read_in():
+    def read_in_and_write():
+        current_player = 'X'
         array = [-1,-1]
         input_data = input("Please enter location of desired placement in the following format: (X,0): ")
         for i in input_data:
@@ -29,9 +24,26 @@ class play:
             elif(i == 'X' or i == 'O'):
                 array[0] = i
         board[int(array[1])] = array[0]
-        print(board)
+        play.print_board_data()
 
+    def check_win():
+        if(board[4] == board[0] and board[4] == board[8]): # diag top left to right
+            print(board[4] + ' WINS!')
+        elif(board[4] == board[6] and board[4] == board[2]): # diag bot left to right
+            print(board[4] + ' WINS!')
+        elif(board[0] == board[1] and board[0] == board[2]): # top left to right
+            print(board[0] + ' WINS!')
+        elif(board[3] == board[4] and board[5] == board[2]): # middle left to right
+            print(board[3] + ' WINS!')
+        elif(board[6] == board[7] and board[6] == board[8]): # bot left to right
+            print(board[6] + ' WINS!')
+        elif(board[0] == board[3] and board[0] == board[6]): # top left down
+            print(board[0] + ' WINS!')
+        elif(board[1] == board[4] and board[1] == board[7]): # middle down
+            print(board[1] + ' WINS!')
+        elif(board[2] == board[5] and board[8] == board[2]): # right down
+            print(board[2] + ' WINS!')
 
 
 # print_board_data()
-play.read_in()
+play.check_win()
